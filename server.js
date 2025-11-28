b329912b831a928af345ddb620316af007eb7f7c
Mmarinovich <martinmarinovich@outlook.com>
2025-11-28 19:34:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 import { sequelize, conectarDB } from './database/index.js';
-import { Producto, Usuario, Venta, ProductoVenta } from './models/index.js';
+import { Usuario } from './models/index.js';
 
 import Endpoints from './config/endpoints.js';
 
@@ -27,7 +27,7 @@ import dashboardRouter from './routes/admin/dashboard.js';
 import { verificarJWTAdmin } from './middleware/auth.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
